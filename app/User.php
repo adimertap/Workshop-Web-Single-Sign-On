c7adbb1c2c77ece0b0b1cd7286d62f6899600c73
adimertaaa <adimerta@student.unud.ac.id>
2021-04-04 19:44:53 +0800

    Progress

--- app/User.php
+++ app/User.php
@@ -5,11 +5,11 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable
 {
-    use Notifiable, HasRoles;
+    use Notifiable;
 
     protected $guard_name = 'api';
 
