e4546037d264b425418b04165eb295c6ba3a81f7
adimertaaa <adimerta@student.unud.ac.id>
2021-06-02 13:54:45 +0800

    gas

--- app/Http/Controllers/Accounting/Receiveable/PemasukanController.php
+++ app/Http/Controllers/Accounting/Receiveable/PemasukanController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\PointOfSales\Laporan\LaporanServiceController;
 use App\Http\Controllers\PointOfSales\Pembayaran\PembayaranSparepartController;
 use App\Model\Marketplace\Transaksi;
 use App\Model\PointOfSales\LaporanPenjualanSparepart;
+use App\Model\PointOfSales\LaporanService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 
@@ -21,10 +22,11 @@ class PemasukanController extends Controller
     {
         $pemasukankasir = LaporanPenjualanSparepart::groupBy('tanggal_laporan','status_jurnal')->selectRaw('SUM(total_tagihan) as grand_total, tanggal_laporan, COUNT(id_laporan) as jumlah_transaksi, status_jurnal')->get();
         $transaksionline = Transaksi::groupBy('tanggal_transaksi')->selectRaw('SUM(harga_total) as total_harga, tanggal_transaksi, COUNT(id_transaksi_online) as jumlah_transaksi_online')->get(); 
+        $laporanservice = LaporanService::groupBy('tanggal_laporan','status_jurnal')->selectRaw('SUM(total_tagihan) as tagihan_total, tanggal_laporan, COUNT(id_laporan_service) as jumlah_service, status_jurnal')->get();
         $today = Carbon::now()->isoFormat('dddd');
         $tanggal = Carbon::now()->format('j F Y');
 
-        return view('pages.accounting.receiveable.pemasukan',['total_pemasukan' => LaporanPenjualanSparepart::sum('total_tagihan'), 'total_pemasukan_online' => Transaksi::where('transaksi_status','DIKIRIM')->sum('harga_total')], compact('today','tanggal','pemasukankasir','transaksionline'));
+        return view('pages.accounting.receiveable.pemasukan',['total_pemasukan' => LaporanPenjualanSparepart::sum('total_tagihan'), 'total_pemasukan_online' => Transaksi::where('transaksi_status','DIKIRIM')->sum('harga_total'),'total_service' => LaporanService::sum('total_tagihan')], compact('today','tanggal','pemasukankasir','transaksionline','laporanservice'));
     }
 
     /**
@@ -62,6 +64,13 @@ class PemasukanController extends Controller
     }
 
     public function Pemasukanonline($tanggal_transaksi)
+    {
+        $online = Transaksi::with(['Detailtransaksi'])->where('tanggal_transaksi', $tanggal_transaksi)->get();
+        
+        return view('pages.accounting.receiveable.detailonline', ['sub_total' => Transaksi::where('tanggal_transaksi', $tanggal_transaksi)->sum('harga_total')], compact('online','tanggal_transaksi'));
+    }
+
+    public function Laporanservice($tanggal_transaksi)
     {
         $online = Transaksi::with(['Detailtransaksi'])->where('tanggal_transaksi', $tanggal_transaksi)->get();
         
