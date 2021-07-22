f51dd69c1e301e2a3279618d6affd5eaf792f2e7
anggakusumap <kusuma_putra@student.unud.ac.id>
2021-07-22 17:10:03 +0800

    Update JenisKendaraanrequest.php

--- app/Http/Requests/FrontOffice/JenisKendaraanrequest.php
+++ app/Http/Requests/FrontOffice/JenisKendaraanrequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\FrontOffice;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class JenisKendaraanRequest extends FormRequest
+class JenisKendaraanrequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
