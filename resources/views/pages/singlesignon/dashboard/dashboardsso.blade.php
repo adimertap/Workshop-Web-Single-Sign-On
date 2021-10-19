3d11223ef8fd517a71eeae23c393bbc07fe7ed4c
anggakusumap <kusuma_putra@student.unud.ac.id>
2021-10-19 18:16:45 +0800

    Update dashboardsso.blade.php

--- resources/views/pages/singlesignon/dashboard/dashboardsso.blade.php
+++ resources/views/pages/singlesignon/dashboard/dashboardsso.blade.php
@@ -8,7 +8,7 @@
             <div class="page-header-content pt-4">
                 @if(Auth::user()->bengkel->status_bayar == "belum_bayar")
                     <div class="alert alert-danger" role="alert"> <i class="fas fa-check"></i>
-                        Bengkel belum melakukan pembayaran
+                        Bengkel belum melakukan pembayaran <a href="" style="font-weight: bold">Bayar Sekarang</a>
                         <button class="close" type="button" data-dismiss="alert" aria-label="Close">
                             <span aria-hidden="true">×</span>
                         </button>
