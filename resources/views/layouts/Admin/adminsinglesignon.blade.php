bb38b21866e8539c5be95089f7769e8c31c708a9
anggakusumap <kusuma_putra@student.unud.ac.id>
2021-12-23 23:42:18 +0800

    Update adminsinglesignon.blade.php

--- resources/views/layouts/Admin/adminsinglesignon.blade.php
+++ resources/views/layouts/Admin/adminsinglesignon.blade.php
@@ -33,8 +33,15 @@
         <div class="small">
             <i class="fa fa-cogs" aria-hidden="true"></i>
             Bengkel
-            <span class="font-weight-500 text-primary"></span>
+            <span class="font-weight-500 text-primary">
             {{ Auth::user()->bengkel->nama_bengkel}}
+
+            @if (Auth::user()->pegawai->cabang != null)
+            {{ Auth::user()->pegawai->cabang->nama_cabang }}
+            @else
+
+            @endif
+            </span>
         </div>
         </form>
         <ul class="navbar-nav align-items-center ml-auto">
@@ -148,8 +155,9 @@
                             Logout
                         </a>
                         <hr>
-                        @if (Auth::user()->pegawai->cabang != null && Auth::user()->pegawai->jabatan->nama_jabatan == 'Owner')
-                        <a class="nav-link"  data-toggle="modal" href="" data-target="#Modalkonfirmasi">
+                        @if (Auth::user()->pegawai->cabang != null && Auth::user()->pegawai->jabatan->nama_jabatan ==
+                        'Owner')
+                        <a class="nav-link" data-toggle="modal" href="" data-target="#Modalkonfirmasi">
                             <div class="nav-link-icon"><i class="fas fa-sign-out-alt"></i></div>
                             Kembali ke Pusat
                         </a>
@@ -170,7 +178,7 @@
             </nav>
         </div>
 
-       
+
         <div class="modal fade" id="Modalkonfirmasi" tabindex="-1" role="dialog"
             aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
@@ -183,7 +191,7 @@
                     <form action="{{ route('set-pusat') }}" method="POST" class="d-inline">
                         @csrf
                         <div class="modal-body">
-                           
+
                             <div class="form-group">Apakah Anda Yakin Menuju Pusat?</div>
                         </div>
                         <div class="modal-footer">
@@ -194,7 +202,7 @@
                 </div>
             </div>
         </div>
-        
+
 
 
         <div id="layoutSidenav_content">
